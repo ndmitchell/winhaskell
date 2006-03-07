cb32d6dd102f7e22c8e2a2b32c3de91770befed4
Neil Mitchell <ndmitchell@gmail.com>
2006-03-07 13:29:22 +0000

    Startcode had bad line endings (thanks to Stephen Eldridge)

--- StartCode.cpp
+++ StartCode.cpp
@@ -1,25 +1,25 @@
-#include "Header.h"
-
-// Shared variables
-HINSTANCE G_hInstance;
-HWND      G_hWnd;
-
-
-INT APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, INT nCmdShow)
-{
-    InitCommonControls();
-    LoadLibrary("RICHED32.DLL");
-
-    // Save application instance
-    G_hInstance = hInstance;
-    ShowMainDialog();
-
-	MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0) > 0)
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-    }
-
-    return 0;
-}
+#include "Header.h"
+
+// Shared variables
+HINSTANCE G_hInstance;
+HWND      G_hWnd;
+
+
+INT APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, INT nCmdShow)
+{
+    InitCommonControls();
+    LoadLibrary("RICHED32.DLL");
+
+    // Save application instance
+    G_hInstance = hInstance;
+    ShowMainDialog();
+
+	MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+    }
+
+    return 0;
+}
